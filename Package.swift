f992f7c0922125ec7ac948a3f6229522570d601c
abinchy <abinchy@hotmail.com>
2023-11-14 19:11:33 +0800

    init

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version: 5.7
+import PackageDescription
+
+let package = Package(
+  name: "XrayKit",
+  platforms: [.iOS(.v12), .macOS(.v12)],
+  products: [
+    .library(name: "XrayKit", targets: ["XrayKit"])
+  ],
+  targets: [
+    .binaryTarget(
+      name: "XrayKit",
+      url: "https://github.com/arror/XrayKit/releases/download/0.1.116/XrayKit.xcframework.zip",
+      checksum: "2916addbb3dec4e7a0449dde91e132b55f421e65cd780bbcd5f8295422195251"
+    )
+  ]
+)
